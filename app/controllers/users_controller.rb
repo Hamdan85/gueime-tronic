897f74c08b0d0d83430691dd1eea99cc5f465ae7
Gabriel Hamdan <ghamdan.eng@gmail.com>
2013-04-26 11:16:05 -0300

    Login Form Design 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
-  def login
-  end
+	def login
+	end
 
-  def register
-  end
+	def register
+	end
 
-  def edit
-  end
+	def edit
+
+	end
+
+	def show
+		@user = User.find_by_username(params[:id])
+    	#@user = User.find(params[:id])
+  	end
 end
